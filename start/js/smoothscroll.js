671ec04e14c90a6aff25ebcc5abe7539173bb485
Andry Sobolev <andrysobolev@users.noreply.github.com>
2016-04-06 14:15:39 +0300

    Fix Bootstrap Tabs

--- start/js/smoothscroll.js
+++ start/js/smoothscroll.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($) {
 // http://css-tricks.com/snippets/jquery/smooth-scrolling/
 // page scroll to an anchor on the same page.
-	$('a[href*=#]:not([href=#]):not([href=#views-bootstrap-carousel-1])').click(function() {
+	$('a[href*=#]:not([href=#]):not([href=#views-bootstrap-carousel-1]):not([data-toggle=tab])').click(function() {
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 		  var target = $(this.hash);
 		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
